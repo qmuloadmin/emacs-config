fa6a71c1d56e24f60e3472caf149190da7b08def
Zach Bullough <zach@ordermygear.com>
2022-02-14 17:40:14 -0600

    minor theme font face fixes

--- init.el
+++ init.el
@@ -231,7 +231,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Iosevka Fixed Medium")))))
+ '(default ((t (family "Iosevka Term Medium"))))
+ '(font-lock-type-face ((t (:inherit modus-themes-bold :family "Iosevka Term Medium")))))
 
 ;; Window resizing configuration
 ;; Set to C-c combined with WASD (capital) controls
